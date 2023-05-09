20d0ffe4866010cd1c90bdab6d01733716dd33b9
Jan Macku <jamacku@redhat.com>
2023-05-09 09:36:10 +0200

    style: use `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,22 @@ use clap::{Parser, Subcommand};
 
 //type Follower = String;
 
-#[derive(Debug, Clone )]
+#[derive(Debug, Clone)]
 struct Follower {
-    link : String,
-    name : String,
-    path: String
+    link: String,
+    name: String,
+    path: String,
 }
 
 // Converts the vector of string returned byt the parser to vector of followers.
 // NOTE: Does not check, whether the length of the input is a multiple of 3, the parser should provide an input of the proper length, but some further check here might be appropriate
-fn unwrap_followers(arg: &Vec<String>) -> Vec<Follower>{
+fn unwrap_followers(arg: &Vec<String>) -> Vec<Follower> {
     let mut rv = Vec::new();
-    for chunk in arg.chunks(3)
-    {
+    for chunk in arg.chunks(3) {
         let path = chunk[0].clone();
         let name = chunk[1].clone();
         let link = chunk[2].clone();
-        rv.push(Follower{link, name, path});
+        rv.push(Follower { link, name, path });
     }
     rv
 }
@@ -31,44 +30,42 @@ enum Commands {
     #[command(long_flag = "install")]
     Install {
         /// TODO: Help text goes here
-        link : String,
+        link: String,
         /// TODO: Help text goes here
-        name : String,
+        name: String,
         /// TODO: Help text goes here
-        path : String,
+        path: String,
         /// TODO: Help text goes here
-        priority : i32,
+        priority: i32,
         /// TODO: Help text goes here
         #[arg(long, num_args=3, value_names=["LINK", "NAME", "PATH"])]
         follower: Vec<String>,
         /// TODO: Help text goes here
-        initscript: Option<String>
+        initscript: Option<String>,
     },
     /// TODO: Help text goes here
     #[command(long_flag = "remove")]
-    Remove{name : String, path: String},
+    Remove { name: String, path: String },
     /// TODO: Help text goes here
     #[command(long_flag = "set")]
-    Set{name : String, path: String},
+    Set { name: String, path: String },
     /// TODO: Help text goes here
     #[command(long_flag = "auto")]
-    Auto{name : String, path: String},
+    Auto { name: String, path: String },
     /// TODO: Help text goes here
     #[command(long_flag = "display")]
-    Display{name : String},
+    Display { name: String },
     /// TODO: Help text goes here
     #[command(long_flag = "config")]
-    Config{name: String}
+    Config { name: String },
 }
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Arguments {
-
     /*
      * Common Options
      */
-
     /// Generate more comments about what alternatives is doing.
     #[arg[long]]
     verbose: bool,
@@ -103,37 +100,44 @@ fn arg_debug_assert() {
 #[test]
 fn arg_test() {
     use clap::CommandFactory;
-    let testing_vec: Vec <_> = "--test".split(" ").collect();
-    let cli = Arguments::command().no_binary_name(true).get_matches_from(testing_vec);
+    let testing_vec: Vec<_> = "--test".split(" ").collect();
+    let cli = Arguments::command()
+        .no_binary_name(true)
+        .get_matches_from(testing_vec);
     assert_eq!(cli.get_flag("test"), true);
 }
 
 #[test]
 fn arg_install() {
     use clap::CommandFactory;
-    let testing_vec: Vec <_> = "--install link name path prio".split(" ").collect();
-    let cli = Arguments::command().no_binary_name(true).get_matches_from(testing_vec);
+    let testing_vec: Vec<_> = "--install link name path prio".split(" ").collect();
+    let cli = Arguments::command()
+        .no_binary_name(true)
+        .get_matches_from(testing_vec);
     assert!(cli.contains_id("install"));
 }
-
 
 fn main() {
     let cli = Arguments::parse();
 
     match cli.command {
-        Commands::Install {ref link,ref name,ref path,ref priority,ref follower, ref initscript} => {
+        Commands::Install {
+            ref link,
+            ref name,
+            ref path,
+            ref priority,
+            ref follower,
+            ref initscript,
+        } => {
             println!("In the Install Brannch!");
             let followers = unwrap_followers(follower);
-            println!("{:?}",followers);
-            println!("Verbose: {:?}",cli.verbose);
-
-        },
-        Commands::Remove {ref name,ref path} => {
+            println!("{:?}", followers);
+            println!("Verbose: {:?}", cli.verbose);
+        }
+        Commands::Remove { ref name, ref path } => {
             println!("In the remove Branch!");
-            println!("Name: {:?}, Path: {:?}",name,path)
-        },
+            println!("Name: {:?}, Path: {:?}", name, path)
+        }
         _ => println!("We've got a problem"),
     };
-
-
 }
