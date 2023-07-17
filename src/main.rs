86510beb91748653650cc00fd68d91d7cbc2e096
jamartis <jamartis@redhat.com>
2023-07-17 14:37:43 +0200

    [WIP] Command line argument parser (#7)
    * WIP: first version of the command line parser
    
    * reiplemment the unwrap_followers function
    
    * build: update Cargo.lock
    
    * style: use `cargo fmt`
    
    * test: update integration test and disable unit tests for now
    
    * typo
    
    * Add more tests
    
    * add the obsolete alias for the follower subcommand
    
    ---------
    
    Co-authored-by: Jan Macku <jamacku@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,150 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Clone)]
+struct Follower {
+    link: String,
+    name: String,
+    path: String,
+}
+
+// Converts the vector of string returned byt the parser to vector of followers.
+// NOTE: Does not check, whether the length of the input is a multiple of 3, the parser should provide an input of the proper length, but some further check here might be appropriate
+fn unwrap_followers(arg: &[String]) -> Vec<Follower> {
+    let mut rv = Vec::new();
+
+    for chunk in arg.chunks(3) {
+        rv.push(Follower {
+            link: chunk[0].clone(),
+            name: chunk[1].clone(),
+            path: chunk[2].clone(),
+        });
+    }
+    rv
+}
+
+//Based on https://linux.die.net/man/8/alternatives
+// TODO add aliases for some now obsolete commands
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// TODO: Help text goes here
+    #[command(long_flag = "install")]
+    Install {
+        /// TODO: Help text goes here
+        link: String,
+        /// TODO: Help text goes here
+        name: String,
+        /// TODO: Help text goes here
+        path: String,
+        /// TODO: Help text goes here
+        priority: i32,
+        /// TODO: Help text goes here
+        #[arg(long, num_args=3, alias="slave", value_names=["LINK", "NAME", "PATH"])]
+        follower: Vec<String>,
+        /// TODO: Help text goes here
+        initscript: Option<String>,
+    },
+    /// TODO: Help text goes here
+    #[command(long_flag = "remove")]
+    Remove { name: String, path: String },
+    /// TODO: Help text goes here
+    #[command(long_flag = "set")]
+    Set { name: String, path: String },
+    /// TODO: Help text goes here
+    #[command(long_flag = "auto")]
+    Auto { name: String, path: String },
+    /// TODO: Help text goes here
+    #[command(long_flag = "display")]
+    Display { name: String },
+    /// TODO: Help text goes here
+    #[command(long_flag = "config")]
+    Config { name: String },
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Arguments {
+    /*
+     * Common Options
+     */
+    /// Generate more comments about what alternatives is doing.
+    #[arg[long]]
+    verbose: bool,
+
+    /// Don't generate any comments unless errors occur. This option is not yet implemented.
+    #[arg[long]]
+    quiet: bool,
+
+    /// Don't actually do anything, just say what would be done. This option is not yet implemented.
+    #[arg[long]]
+    test: bool,
+
+    /// Specifies the alternatives directory, when this is to be different from the default.
+    #[arg(long, value_name = "alt_dir")]
+    altdir: Option<String>,
+
+    /// Specifies the administrative directory, when this is to be different from the default.
+    #[arg(long, value_name = "admin_dir")]
+    admindir: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Arguments::parse();
+
+    match cli.command {
+        Commands::Install {
+            ref link,
+            ref name,
+            ref path,
+            ref priority,
+            ref follower,
+            ref initscript,
+        } => {
+            println!("In the Install Branch!");
+            println!("Name: {:?}, Path: {:?}", name, path);
+            let followers = unwrap_followers(follower);
+            println!("{:?}", followers);
+            println!("Verbose: {:?}", cli.verbose);
+        }
+        Commands::Remove { ref name, ref path } => {
+            println!("In the remove Branch!");
+            println!("Name: {:?}, Path: {:?}", name, path)
+        }
+        _ => println!("We've got a problem"),
+    };
+}
+
+// TODO: more tests!
+#[cfg(test)]
+pub mod cli {
+    use super::*;
+    use clap::CommandFactory;
+
+    #[test]
+    #[ignore = "FIXME"]
+    fn arg_debug_assert() {
+        Arguments::command().debug_assert();
+    }
+
+    #[test]
+    #[ignore = "FIXME"]
+    fn arg_test() {
+        let testing_vec: Vec<_> = "--test".split(" ").collect();
+        let cli = Arguments::command()
+            .no_binary_name(true)
+            .get_matches_from(testing_vec);
+        assert_eq!(cli.get_flag("test"), true);
+    }
+
+    #[test]
+    #[ignore = "FIXME"]
+    fn arg_install() {
+        let testing_vec: Vec<_> = "--install link name path prio".split(" ").collect();
+        let cli = Arguments::command()
+            .no_binary_name(true)
+            .get_matches_from(testing_vec);
+        assert!(cli.contains_id("install"));
+    }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,10 +1,50 @@
 use assert_cmd::Command;
 
 #[test]
-fn hello_world() -> Result<(), Box<dyn std::error::Error>> {
+fn alternatives_no_parameters() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("alternatives")?;
 
-    cmd.assert().success().code(0).stdout("Hello, world!\n");
+    cmd.assert().failure().code(2).stderr(
+"Usage: alternatives [OPTIONS] <COMMAND>
+
+Commands:
+  install, --install  TODO: Help text goes here
+  remove, --remove    TODO: Help text goes here
+  set, --set          TODO: Help text goes here
+  auto, --auto        TODO: Help text goes here
+  display, --display  TODO: Help text goes here
+  config, --config    TODO: Help text goes here
+  help                Print this message or the help of the given subcommand(s)
+
+Options:
+      --verbose               Generate more comments about what alternatives is doing
+      --quiet                 Don't generate any comments unless errors occur. This option is not yet implemented
+      --test                  Don't actually do anything, just say what would be done. This option is not yet implemented
+      --altdir <alt_dir>      Specifies the alternatives directory, when this is to be different from the default
+      --admindir <admin_dir>  Specifies the administrative directory, when this is to be different from the default
+  -h, --help                  Print help
+  -V, --version               Print version
+");
+
+    Ok(())
+}
+
+#[test]
+fn alternatives_install_no_parameters() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("alternatives")?;
+
+    cmd.arg("install").assert().failure().code(2).stderr(
+        "error: the following required arguments were not provided:
+  <LINK>
+  <NAME>
+  <PATH>
+  <PRIORITY>
+
+Usage: alternatives {install|--install} <LINK> <NAME> <PATH> <PRIORITY> [INITSCRIPT]
+
+For more information, try '--help'.
+",
+    );
 
     Ok(())
 }
