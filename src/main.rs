1ed7c126281b77f62cca24999e3a803af0509a2d
Jakub Martisko <jamartis@redhat.com>
2023-09-27 13:25:45 +0200

    Add: conversion of --install command to yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use clap::{Parser, Subcommand};
+use serde::{Serialize, Deserialize};
+use serde_yaml::Value;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Follower {
     link: String,
     name: String,
@@ -20,6 +25,113 @@ fn unwrap_followers(arg: &[String]) -> Vec<Follower> {
         });
     }
     rv
+}
+
+// The following two structs form the content of the the DB files.
+// FIXME Rename these two structs to something more descriptive
+// This structure stores a singel alternative
+/*#[derive(Serialize,Deserialize,Debug,Clone)]
+struct Alternative {
+    link:  String,
+    path:  String,
+    priority:  i32,
+    followers: Option<Vec<Follower>>
+}*/
+
+
+#[derive(Serialize,Deserialize,Debug,Clone)]
+struct WithFollowers {link: String, path: String, priority: i32, followers: Vec<Follower>}
+#[derive(Serialize,Deserialize,Debug,Clone)]
+struct WithoutFollowers {link: String, path: String, priority: i32}
+
+#[derive(Serialize,Deserialize,Debug,Clone)]
+#[serde(untagged)]
+enum Alternative {
+    WithFollowers(WithFollowers),
+    WithoutFollowers(WithoutFollowers),
+}
+
+fn create_alternative (link: String, path: String, priority: i32, followers: Vec<Follower>) -> Alternative{
+    match  followers.len() {
+        0 => return Alternative::WithoutFollowers(WithoutFollowers {link, path, priority}),
+        _ => return Alternative::WithFollowers(WithFollowers {link, path, priority, followers}),
+    }
+}
+
+
+#[derive(Serialize,Deserialize,Debug,Clone)]
+struct BuiltIn {name: String, mode: String, manualPath: String, alternatives: Vec<Alternative>}
+#[derive(Serialize,Deserialize,Debug,Clone)]
+struct DropIn {name: String, alternatives: Vec<Alternative>}
+
+//This structure contains the header + the vector of alternatives
+#[derive(Serialize,Deserialize,Debug,Clone)]
+#[serde(untagged)]
+enum AlternativeGroup {
+    BuiltIn (BuiltIn),
+    DropIn  (DropIn),
+}
+
+
+fn create_dropin (name: String, alternatives: Vec<Alternative>) -> AlternativeGroup {
+    return AlternativeGroup::DropIn(DropIn {name, alternatives});
+}
+
+fn group_to_builtin (drop_in: AlternativeGroup) -> AlternativeGroup {
+    match drop_in {
+        AlternativeGroup::DropIn (group) => return AlternativeGroup::BuiltIn(BuiltIn {name : group.name, mode : "Auto".to_string(), manualPath : "".to_string(),  alternatives : group.alternatives}),
+        AlternativeGroup::BuiltIn (group) => return AlternativeGroup::BuiltIn(BuiltIn {name : group.name, mode : group.mode, manualPath : group.manualPath, alternatives : group.alternatives}),
+    }
+}
+
+/*
+ *
+# naming based on: https://linux.die.net/man/8/alternatives# generic name for the group/maste symlink
+- name: /usr/bin/editor
+  mode: manual #manual or auto
+# override info for the manual mode
+  manual_path: /usr/bin/emacs
+  group:
+   - path: /usr/bin/vim
+     link: sl_to_vim
+     priority: 10
+     follower:
+       - link: foo1
+         name: foo1
+         path: foo1
+       - link: foo2
+         name: foo2
+         path: foo2
+   - path: /usr/bin/emacs
+     link: sl_to_emacs
+     priority: 9- name: version_control
+  mode: auto
+  manual_path:
+  group:
+  - path: /usr/bin/path
+    link: sl_to_git
+    priority: 100
+  - path: /usr/bin/svn
+    link: sl_to_subversion
+    priority: 10
+ *
+ *
+ */
+
+//reads a single DB file and returns a list of of structs for each NAME entry
+fn read_config(path: String) {
+    let content = Value::String(fs::read_to_string(path).expect("File error"));
+    let rv: AlternativeGroup = serde_yaml::from_value(content).unwrap();
+}
+
+fn write_config(path: String, content: &[&AlternativeGroup]) -> std::io::Result<()>{
+    let yaml = serde_yaml::to_string(content).expect("Parsing error");
+    fs::write(path,yaml.as_bytes()).expect("Writing to file");
+    Ok(())
+}
+
+// add an alternative to the ones read from the db
+fn add_alternative(){
 }
 
 //Based on https://linux.die.net/man/8/alternatives
@@ -105,7 +217,13 @@ fn main() {
             println!("In the Install Branch!");
             println!("Name: {:?}, Path: {:?}", name, path);
             let followers = unwrap_followers(follower);
-            println!("{:?}", followers);
+            let alt = create_alternative(link.to_string(),path.to_string(),*priority,followers);
+            let alts = group_to_builtin (create_dropin (name.to_string(), [alt.clone()].to_vec()));
+
+            let res = serde_yaml::to_string(&[&alts]);
+            write_config("/tmp/foo".to_string(), &[&alts]);
+            println!("{:?}", alt);
+            println!("{:?}", res);
             println!("Verbose: {:?}", cli.verbose);
         }
         Commands::Remove { ref name, ref path } => {
